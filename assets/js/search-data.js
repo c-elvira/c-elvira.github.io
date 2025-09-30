40a2812778deab1ec8b4163055899dfb98de19dd
c-elvira <8436253+c-elvira@users.noreply.github.com>
2025-09-30 13:30:00 +0000

    Deploying to gh-pages from @ c-elvira/c-elvira.github.io@ac8e1b4a9f4af10805f4cfb9a292a99e2ed51f5f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,20 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
@@ -405,16 +391,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-paper-ryu-safe-ball-construction-for-optimization-has-been-accepted-in-the-open-journal-of-mathematical-optimization-a-real-hadouken-for-safe-sets-check-it-out",
+          title: 'Our paper RYU: Safe Ball Construction for Optimization has been accepted in the...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -466,14 +444,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%63%6C%65%6D%65%6E%74.%65%6C%76%69%72%61@%63%65%6E%74%72%61%6C%65%73%75%70%65%6C%65%63.%66%72", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://orcid.org/0000-0001-5316-2191", "_blank");
         },
       },{
         id: 'social-rss',
@@ -487,14 +465,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=2LSEQYcAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
